bc4399fc69c6ed531d1a13d21ca7c13fcad5351c
fhdufhdu <32770312+fhdufhdu@users.noreply.github.com>
2020-08-10 18:42:33 +0900

    Update test.js

--- test.js
+++ test.js
@@ -11,10 +11,10 @@ function review_print(){
 				var review_cnt = json_data.list[cnt++].review_cnt;
 				var prod_name = json_data.list[cnt++].product_name;
 				var price = json_data.list[cnt++].product_price;
-				document.getElementById("review_list").innerHTML +='<li><div class="review_context"><div class="div_r_image"><div><image src="https://shopping-phinf.pstatic.net/main_8747453/8747453599.6.jpg?type=f140" class="review_image"/></div></div><div class="div_r_content"><div><p class="review_cnt_text">리뷰 갯수</p><p class="review_cnt">'+review_cnt+'건</p><p class="prod_name">'+prod_name+'</p><p class="price">'+price+'</p></div></div></div></li>';
+				document.getElementById("review_list").innerHTML +='<li><div class="review_context"><div class="div_r_image"><div><image src="https://shopping-phinf.pstatic.net/main_8747453/8747453599.6.jpg?type=f140" class="review_image" id="thumbnail_image"/></div></div><div class="div_r_content"><div><p class="review_cnt_text">리뷰 갯수</p><p class="review_cnt">'+review_cnt+'건</p><p class="prod_name">'+prod_name+'</p><p class="price">'+price+'</p></div></div></div></li>';
+				var image_width = (window.innerWidth/2)*0.7;
+				document.getElementById("thumbnail_image").width = image_width;
 			}		
      		}
     	})
-	var image_width = (window.innerWidth/2)*0.7
-	document.getElementById("")
 }
